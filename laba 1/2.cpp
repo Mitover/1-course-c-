c0d77ed94de75c13cecc8594c95074924f6b6754
Mitover <76473397+Mitover@users.noreply.github.com>
2024-07-22 11:15:53 +0500

    Update 2.cpp

--- laba 1/2.cpp
+++ laba 1/2.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 void main() 
 { 
-    int a,b,c,x; 
-    cout<<"Input a!=0"; 
-    cin>>a; 
-    cout<<"Input b"; 
-    cin>>b; 
-    cout<<"Input c"; 
-    cin>>c; 
-    if((b*b-4*a*c)>=0) 
+    int a, b, c, x; 
+    cout << "Input a!=0"; 
+    cin >> a; 
+    cout << "Input b"; 
+    cin >> b; 
+    cout << "Input c"; 
+    cin >> c; 
+    if( (b*b-4*a*c) >= 0) 
     { 
-      x=(-1*b+sqrt(b*b-4*a*c))/(2*a); 
-      cout<<"First root="<<x<<endl; 
-      x=(-1*b-sqrt(b*b-4*a*c))/(2*a); 
-      cout<<"Second root="<<x<<endl; 
+      x = (-1 * b+  sqrt(b*b-4*a*c)) / (2 * a); 
+      cout << "First root=" << x << endl; 
+      x = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a); 
+      cout << "Second root="<< x << endl; 
      } 
      else 
     { 
-	cout<<"not roots"<<endl; 
+	cout << "not roots" << endl; 
     } 
 system ("pause");
 }
