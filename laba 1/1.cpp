27aae691d50d801c8d3b65127087bcc2b045b007
Mitover <76473397+Mitover@users.noreply.github.com>
2024-07-24 09:09:56 +0500

    Update 1.cpp

--- laba 1/1.cpp
+++ laba 1/1.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 void main()
 {
-	double a,b;
-	int x,y,z;
-	cout<<" Input x, y , z"<<endl;
-	cin>>x;
-	cin>>y;
-	cin>>z;
-	a=2*cos(x-(p/4))/(1/2+pow(sin(y),2))+acos(x+(p/2));
-	b=1+(pow(z,2)/3+pow(z,2)/5);
-	cout<<a<<endl;
-	cout<<b<<endl;
+	double a, b;
+	int x, y ,z;
+	cout << " Input x, y , z" << endl;
+	cin >> x;
+	cin >> y;
+	cin >> z;
+	a = 2 * cos(x-(p/4)) / (1 / 2 + pow(sin(y), 2)) + acos(x + (p/2));
+	b = 1 + (pow(z, 2)/3 + pow(z, 2)/5);
+	cout << a << endl;
+	cout << b << endl;
 	system("pause");
 }
